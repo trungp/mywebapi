2a8b23f80a71a110174947419c63901469f37345
Trung Pham <trungp2007@gmail.com>
2019-11-29 23:29:09 +0800

    Sample commit
    Sample commit

--- src/main/java/com/ms/sample/mywebapi/Application.java
+++ src/main/java/com/ms/sample/mywebapi/Application.java
@@ -3,7 +3,7 @@ package com.ms.sample.mywebapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
-import java.util.Date();
+import java.util.Date;
 
 @SpringBootApplication
 @RestController
